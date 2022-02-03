6c84ebce6474ba8d1a264cb9c962dee51ad2723d
jose.duque <jose.duque@LAP0870.internal.overactiveinc.com>
2022-02-02 20:07:57 -0500

    Test

---
+++ SeleniumCursoEnero/src/test/java/com/java/Ejercicio_1.java
@@ -0,0 +1,29 @@
+package com.java;
+
+public class Ejercicio_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double var1 = 2;
+		double var2 = 4;
+		double var3 = 7;
+		
+		double suma = var1 + var2;
+		double resta = var3 - var1;
+		double mult = var1 * var3;
+		double div = var2/var1;
+		
+		String in1 = "Cool";
+		String in2 = "Selenium con Java ";
+		String in3 = "Curso de ";
+		String in4 = "Testers";
+		
+		System.out.println("El resultado de la suma es: " + suma);
+		System.out.println("El resultado de la resta es: " + resta);
+		System.out.println("El resultado de la multiplicación es: " +mult);
+		System.out.println("El resultado de la división es: " + div);
+		
+		System.out.println(in3 + in2 + in1 + in4);
+	}
+
+}
